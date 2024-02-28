8257186e2fe0eb2f5c6f9ed0a7d28ff27c3983c4
David Moser <damoser@schueler.hakzell.at>
2024-02-28 21:58:50 +0100

    add disabling button, add execute Notify

--- Logbook.LogBookApp/ViewModels/MainViewModel.cs
+++ Logbook.LogBookApp/ViewModels/MainViewModel.cs
@@ -12,55 +12,101 @@ namespace Logbook.LogBookApp.ViewModel
 {
     public partial class MainViewModel(IRepository repository) : ObservableObject
     {
+
         public string Header => "Fahrtenbuch";
+
+        public object Entries { get; private set; }
 
         IRepository _repository = repository;
 
-        // nichts anderes wie eine Liste, mit einem zusätzlichen Feature, welches die Oberfläche informiert
+
         [ObservableProperty]
-        ObservableCollection<Lib.Entry> _entries = [];
+        ObservableCollection<Logbook.Lib.Entry> _ent = [];
+
+        #region Properties
+
+        [ObservableProperty]
+        DateTime _start = DateTime.Now;
+
+        [ObservableProperty]
+        DateTime _end = DateTime.Now;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(AddCommand))]
+        string _description = string.Empty;
+
+
+        [ObservableProperty]
+        string _numberPlate = string.Empty;
+
+        [ObservableProperty]
+        int _startKM = 0;
+
+        [ObservableProperty]
+        int _endKM = 0;
+
+        [ObservableProperty]
+        string _from = string.Empty;
+
+        [ObservableProperty]
+        string _to = string.Empty;
+
+        #endregion
+
+
 
         [RelayCommand]
         void LoadData()
         {
-            try
-            {
-                var entries = _repository.GetAll();
+            var entries = _repository.GetAll();
 
-                if (entries != null)
-                {
-                    foreach (var entry in entries)
-                    {
-                        Entries.Add(entry);
-                    }
-                }
-            }
-            catch (Exception ex)
+            foreach (var entry in entries)
             {
-                Console.WriteLine(ex.Message);
+                Ent.Add(entry);
             }
         }
 
-        [RelayCommand]
+        private bool CanAdd => this.Description.Length > 0;
+
+
+
+
+        [RelayCommand(CanExecute = nameof(CanAdd))]
         void Add()
         {
-            Lib.Entry entrySaalfelden = new(DateTime.Now.AddDays(3),
-                                  DateTime.Now.AddDays(3).AddMinutes(20),
-                                  25500,
-                                  25514,
-                                  "ZE-XY123",
-                                  "Zell am See",
-                                  "Saalfelden")
-            {
-                Description = "Fahrt nach Saalfelden"
-            };
+            /*
+            Lib.Entry entrySaalfelden = new(
 
-            var result = _repository.Add(entrySaalfelden);
-            if(result == true)
+            DateTime.Now.AddDays(3),
+            DateTime.Now.AddDays(3).AddMinutes(20),
+            25500, 25514,
+            "ZE-XY123",
+            "Zell am See",
+            "Saalfelden");
+            */
+
+            Lib.Entry entry = new(this.Start, this.End, this.StartKM, this.EndKM, this.NumberPlate, this.From, this.To);
+
+            if (this.Description.Length > 0)
             {
-                this.Entries.Add(entrySaalfelden);
+                entry.Description = this.Description;
             }
 
+            var result = _repository.Add(entry);
+
+            if (result)
+            {
+                this.Ent.Add(entry);
+                this.Description = string.Empty;
+                this.From = "";
+                this.To = "";
+                this.StartKM = this.EndKM;
+                this.EndKM = 0;
+            }
+
+
         }
+
+
     }
 }
